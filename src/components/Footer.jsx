ac1cf265ef6af2bf199119548053db632d48d44d
chriszam30 <czambranovillavicenc@byupathway.edu>
2025-08-22 10:26:49 -0500

    Crear dev

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,28 +9,7 @@ export function Footer(){
             </a>
             . All Rights Reserved.
           </span>
-          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" class="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
+          
         </div>
       </footer>
     )
